6b9904e837b4c4934557a4e2869e702f6993fa4e
MiguelSBat <36421049+MiguelSBat@users.noreply.github.com>
2018-02-21 16:39:43 +0100

    Correción menor a announcement

--- D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/repositories/AnnouncementRepository.java
+++ D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/repositories/AnnouncementRepository.java
@@ -12,10 +12,10 @@ import domain.Announcement;
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
 
-	@Query("select a from Rendezvous r join r.announcements a where r.id=?1")
+	@Query("select a from Announcement a where a.rendezvous.id=?1")
 	Collection<Announcement> findByRendezvousId(int id);
 
-		//select a from RSVP r join r.rendezvous.announcements a
-			@Query("select a from RSVP r join r.rendezvous.announcements a where r.user.id=?1")
-			Collection<Announcement> findByUserRSVPId(int id);
+	//select a from RSVP r join r.rendezvous.announcements a
+	@Query("select a from RSVP r join r.rendezvous.announcements a where r.user.id=?1")
+	Collection<Announcement> findByUserRSVPId(int id);
 }
