8d9d2b3fa17351a5ec0b0229000958d9b7619da8
JorgeManuelMolina <36421174+JorgeManuelMolina@users.noreply.github.com>
2018-02-21 18:23:46 +0100

    añadido query dashboard

--- D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/repositories/UserRepository.java
+++ D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/repositories/UserRepository.java
@@ -2,11 +2,17 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+	// Dashboard -----------------------------------------------------------
+	//TODO pasar a servicio y comprobar despues de populated
+	@Query("select count(u)*100/(select count(ru) from User ru where ru.rendezvouses.size=0) from User u where u.rendezvouses.size>0")
+	Double averageUserRendezvousvsUserNoRendezvous();
 
 }
