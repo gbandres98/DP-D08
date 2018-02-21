2d3a97568b8361f99ad3b88dabbcab0ed6a16946
JorgeManuelMolina <36421174+JorgeManuelMolina@users.noreply.github.com>
2018-02-21 18:58:24 +0100

    Añadido fecha de nacimiento, como DATE, modificar donde necesiteis

--- D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/domain/Actor.java
+++ D08 - Lessons learnt/Item 4/AcmeRendezvous/src/main/java/domain/Actor.java
@@ -1,16 +1,21 @@
 
 package domain;
 
+import java.util.Date;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import security.UserAccount;
 
@@ -23,6 +28,7 @@ public class Actor extends DomainEntity {
 	private String	postalAddress;
 	private String	phoneNumber;
 	private String	emailAddress;
+	private Date	birthDate;
 
 
 	@NotBlank
@@ -66,6 +72,17 @@ public class Actor extends DomainEntity {
 
 	public void setEmailAddress(final String emailAddress) {
 		this.emailAddress = emailAddress;
+	}
+
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	public Date getBirthDate() {
+		return this.birthDate;
+	}
+
+	public void setBirthDate(final Date moment) {
+		this.birthDate = moment;
 	}
 
 
